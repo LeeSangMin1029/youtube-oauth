91fbcd0c3a35595257a39e7fe77782a6f8e24a48
LeeSangMin1029 <sangmins39@gmail.com>
2023-02-08 18:12:29 +0900

    prettier, eslint 적용

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,9 @@
-import express, { Request, Response, NextFunction } from "express";
-
+import express, { Request, Response, NextFunction } from 'express';
 const app = express();
 const host = 5000;
 
-app.get("/", (req: Request, res: Response, next: NextFunction) => {
-  res.send("home");
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.send('home');
 });
 
 app.listen(host, () => {
