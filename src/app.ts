cb402929be15474365f7e62ee178d326a53e7d0b
LeeSangMin1029 <sangmins39@gmail.com>
2023-02-08 17:48:16 +0900

    프로젝트 초기 설정

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response, NextFunction } from "express";
+
+const app = express();
+const host = 5000;
+
+app.get("/", (req: Request, res: Response, next: NextFunction) => {
+  res.send("home");
+});
+
+app.listen(host, () => {
+  console.log(`
+  ################################################
+  🛡️  Server listening on port: ${host}
+  ################################################
+`);
+});
