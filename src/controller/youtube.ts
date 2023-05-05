e508fa0aad274d2e42c18e1c9e729bff3750d53f
LeeSangMin1029 <sangmins39@gmail.com>
2023-05-06 00:13:05 +0900

    youtube api 데이터 불러오기
    /api/youtube/videos
    해당 경로로 데이터를 요청하면 parameter에 지정한 대로 결과 응답

--- src/controller/youtube.ts
+++ src/controller/youtube.ts
@@ -1,5 +1,29 @@
 import { Response } from 'express';
+import { oauth2Client, setGoogleAuth } from '@/utils/googleauth';
+import { google, youtube_v3 } from 'googleapis';
+import { getValidatedToken } from '@/utils/token';
+import { updateUser } from '@/service/auth';
 
 export const getAPIVideos = async (_: any, res: Response) => {
-  res.json('hello');
+  const { access_token, refresh_token, googleID } = res;
+  const validToken = await getValidatedToken(access_token!, refresh_token!);
+  if (validToken && access_token !== validToken) {
+    oauth2Client.setCredentials({ access_token: validToken });
+    await updateUser(googleID!, validToken);
+  } else {
+    oauth2Client.setCredentials({ access_token: access_token });
+  }
+  setGoogleAuth();
+  const youtube = google.youtube('v3');
+  const listParams: youtube_v3.Params$Resource$Videos$List = {
+    part: ['snippet', 'player', 'statistics'],
+    chart: 'mostPopular',
+    maxWidth: 1280,
+    maxHeight: 714,
+    fields:
+      'items(id,snippet(channelId,channelTitle,publishedAt,thumbnails(maxres),title),player,statistics(viewCount))',
+  };
+  const videos = await youtube.videos.list(listParams);
+  const items: youtube_v3.Schema$VideoListResponse = videos.data;
+  res.json(items);
 };
